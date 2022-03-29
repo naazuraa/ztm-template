285fe2baacca8d114308987ee994fa6f5a34bc50
nazura <nazura97@gmail.com>
2022-03-30 01:52:10 +0800

    updated homepage with image

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,9 @@
 <template>
   <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
     <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+      <h1 class="text-5xl">Welcome to Lead Reports Search Engine</h1>
+      <p class="mt-10">Let us know what you are looking for.</p>
+      <center><img src="https://img.freepik.com/free-vector/search-concept-landing-page_52683-20655.jpg?t=st=1648575212~exp=1648575812~hmac=ee73e9575b2787443d31b7bfe04ea31693ff4ee93c28ac73796ccade6ed7934c&w=900" alt="Search image" style="width:600px;height:400px;"></center>
     </div>
   </div>
 </template>
