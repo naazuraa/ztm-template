8f754bb182496dd0de776c69aa5b7b90a2751e5b
Nazura Ramli <nazura97@gmail.com>
2022-04-03 10:14:23 +0800

    initial commit

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,35 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+  <div>
+    <div id='header' class="bg-[url('/hero-bg.jpg')] px-20">
+      <!-- <p>This is a hero section.</p> </!-->
+      <div id='menu' class="grid grid-cols-2 py-5">
+        <div id='menu-left'>
+          <img src="logo.png" class= "w-[150px]" alt="" />
+        </div>
+
+        <div id='menu-right'>
+  <!-- ul is unordered list, to create menu item. li is list items</p> </!-->
+          <ul class="grid grid-cols-4 font-oswald text-lg text-white uppercase pt-5">
+            <li>About Us</li>
+            <li>Our Menu</li>
+            <li>Locations</li>
+            <li>Contacts</li>
+          </ul>
+        </div>
+      </div>
+      <div class="grid grid-cols-2 pt-10 pb-20">
+        <img src="hero-burger.png" class="pr-8 mt-10" alt ="" />
+        <div>
+          <h1 class="text-8xl text-white text-center font-lilita uppercase">Classic Burger</h1>
+          <h2 class="text-6xl text-white text-center font-lilita uppercase my-5">With Onion</h2>
+          <p class="text-white font-roboto font-light text-center">Our signature burger now comes with onion and cheese. You can customize it to your likings. Freshly made to order, all in a Brioche style bun.</p>
+        </div>
+      </div>
+    </div>
+    <div id='section-1' class="text-center bg-[url('/three-burgers.jpg')] pb-60 bg-cover">
+        <h1 class="text-6xl uppercase font-oswald text-center pt-20 px-24">The burger tastes better when you eat it with family</h1>
+        <p class="font-roboto font-light text-gray-500 text-center text-lg py-6 px-28">Spend quality time with your family and eat an affordable, nutritious and delicious meal together. Not all burgers are fast food. All of our burgers are handmade to orders for you and your family members to enjoy.</p>
+        <button class="font-oswald uppercase bg-red-500 text-white py-3 px-8">Explore Full Menu</button>
     </div>
   </div>
 </template>
