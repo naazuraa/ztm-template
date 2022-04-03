a5c8bcf8653ad51d70549209a2c18c40007fdc88
Nazura Ramli <nazura97@gmail.com>
2022-04-03 10:43:50 +0800

    update files

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,23 @@
 <template>
-  <div class="flex h-screen bg-slate-600 h-[100vh] align-middle text-white text-center">
-    <div class="m-auto">
-      <h1 class="text-5xl">Welcome to Zero to Marketing!</h1>
-      <p class="mt-5">Turn your ideas into working web applications.</p>
+  <div id='header' class="bg-[url('/green-bg.jpg')] px-20">
+    <!-- <p>This is a hero section.</p> </!-->
+    <div id='menu' class="grid grid-cols-2 py-5">
+      <div id='menu-left'>
+        <img src="logo2.png" class= "w-[100px]" alt="" />
+      </div>
+
+      <div id='menu-right'>
+<!-- ul is unordered list, to create menu item. li is list items</p> </!-->
+        <ul class="grid grid-cols-4 font-oswald text-lg text-white uppercase pt-5">
+          <li>About</li>
+          <li>Contact Us</li>
+          <li>Sign In</li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="grid grid-cols-2">
+      <img src="greensearch.png" alt="" />
     </div>
   </div>
 </template>
